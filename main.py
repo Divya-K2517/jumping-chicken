fe4f753a46eb242d3abdd3ed8729eda7198d9eef
Divya-K2517 <115424525+Divya-K2517@users.noreply.github.com>
2024-12-25 23:53:09 -0500

    added scores, enhanced collision

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import random 
 
 pygame.init()
-WIDTH, HEIGHT = 400, 600
+WIDTH, HEIGHT = 500, 600
 BASE_SPEED = 2
 game_speed = BASE_SPEED
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -17,14 +17,27 @@ class Chicken ():
     def __init__(self):
         self.image = pygame.image.load("jumping chicken/chicken.png")
         self.image = pygame.transform.scale(self.image, (70,70))
+        self.image2 = pygame.image.load("jumping chicken/leftchicken.png")
+        self.image2 = pygame.transform.scale(self.image2, (70,70))
         self.rect = self.image.get_rect()
         self.rect.bottom = HEIGHT
         self.rect.left = WIDTH/2
         self.jumping = False
         self.jvelocity = 0
-        self.gravity = 1.5
+        self.gravity = 1.2
+        self.score = 0
+        self.high_score = 0
+        self.font = pygame.font.Font("T-rex game/font.ttf", 15)
+        self.facing_right = True
     def draw(self):
-        screen.blit(self.image, self.rect)
+        if self.facing_right:
+            screen.blit(self.image, self.rect)
+        else:
+            screen.blit(self.image2,self.rect)
+        score_text = self.font.render("Score: " + str(int(self.score)), True, (0,0,0))
+        screen.blit(score_text, (WIDTH-130, 20))
+        high_score_text = self.font.render("High score: " + str(int(self.high_score)), True, (0,0,0))
+        screen.blit(high_score_text, (WIDTH-130, 0))
     def update(self):
         global game_over
         if not game_over:
@@ -32,9 +45,11 @@ class Chicken ():
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
                 if self.rect.left > 0:
                     self.rect.left -= 5
+                    self.facing_right = False
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 if self.rect.right < WIDTH:
                     self.rect.right += 5
+                    self.facing_right = True
             if keys[pygame.K_UP] or keys[pygame.K_w]:
                 self.jumping = True
                 self.jvelocity = 15
@@ -56,32 +71,48 @@ class Platform(pygame.sprite.Sprite):
         self.image = pygame.Surface((w,15))
         self.image.fill((17,90,44))
         self.rect = self.image.get_rect(topleft=(x, 0))
+        self.landed_on = False
     def draw(self, surface):
         surface.blit(self.image, self.rect)
     def update(self):
         global game_speed
+        if self.rect.bottom > HEIGHT - 20:
+            if abs(chicken.rect.bottom - self.rect.top) < 10:
+                global game_over
+                game_over = True
         self.rect.bottom += game_speed
         if self.rect.bottom > HEIGHT:
             self.kill()
         
 def checkcollision(p,c):
-    landing_tolerance = 5
+    landing_tolerance = 7
     if p.rect.top - landing_tolerance <= c.rect.bottom <= p.rect.top + landing_tolerance and c.rect.right >= p.rect.left and c.rect.left <= p.rect.right:
         print("collision detected")
         c.rect.bottom = p.rect.top
         c.velocity =0
         c.jumping = False
+        if not p.landed_on:
+            c.score += 1
+            if c.score > c.high_score:
+                c.high_score += 1
+            p.landed_on = True
 
-def game_over_check():
+def game_over_screen():
     font = pygame.font.Font("jumping chicken/font.ttf", 40)
     text = font.render("GAME OVER", True, (255,0,0))
-    screen.blit(text, (WIDTH/3, HEIGHT/3))
+    screen.blit(text, (WIDTH/4, HEIGHT/3))
     restart_font = pygame.font.Font("jumping chicken/font.ttf", 20)
     restart_text = restart_font.render("press "+ "'r'" + " to restart game", True, (255,0,0))
-    screen.blit (restart_text,(WIDTH/3, HEIGHT/3 + 50))
+    screen.blit (restart_text,(WIDTH/5, HEIGHT/3 + 50))
     chicken.rect.bottom = HEIGHT
     global game_over 
     game_over = True
+
+def spawn_platform():
+    new_platform = Platform(random.randint(0,WIDTH-int(WIDTH/4)), random.randint(chicken.rect.width,int(WIDTH/5)))
+    while pygame.sprite.spritecollideany(new_platform, platforms):
+        new_platform = Platform(random.randint(0,WIDTH-int(WIDTH/4)), random.randint(chicken.rect.width,int(WIDTH/5)))
+    platforms.add(new_platform)
 
 #objects
 chicken = Chicken()
@@ -105,12 +136,12 @@ while running:
     if not game_over:
         if len(platforms) < 3:
             if len(platforms) == 0:
-                platforms.add(Platform(random.randint(0,WIDTH-int(WIDTH/4)), random.randint(chicken.rect.width,int(WIDTH/3))))
+                spawn_platform()
             else:
                 highest_p = min(platforms, key=lambda p: p.rect.top).rect.top
                 highest_p -= 20
                 if highest_p >= HEIGHT/4:
-                    platforms.add(Platform(random.randint(0,WIDTH-int(WIDTH/4)), random.randint(chicken.rect.width,int(WIDTH/4))))
+                    spawn_platform()
                 
     platforms.draw(screen)
     platforms.update()
@@ -118,10 +149,10 @@ while running:
     for p in platforms:
         checkcollision(p, chicken)
 
-    game_speed += 0.005
+    game_speed += 0.008
 
     if chicken.rect.bottom > HEIGHT or game_over:
-        game_over_check()
+        game_over_screen()
     
     #resetting game
     keys = pygame.key.get_pressed()
@@ -130,6 +161,8 @@ while running:
         platforms.empty()
         game_over = False
         chicken.reset()
+        chicken.score = 0
+        chicken.facing_right = True
         platforms.add(Platform(random.randint(0,WIDTH-int(WIDTH/4)), random.randint(chicken.rect.width,int(WIDTH/4))))
         
 
